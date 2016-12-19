f8b2a18596210e2e8a16720ef1920b8299ca9e50
zhangpengcheng <zhangpch666@163.com>
2016-12-19 10:12:18 +0800

    flask form

--- app/views.py
+++ app/views.py
@@ -1,8 +1,21 @@
-from flask import render_template
+from flask import render_template,flash,redirect
 from app import app
+from .forms import loginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'zpc'}
     return render_template('index.html', title='Home', user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = loginForm()
+    if form.validate_on_submit():
+        flash('Login requested for openid="' + form.openid.data + '",remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form= form,
+                           providers = app.config['OPENID_PROVIDERS'])
